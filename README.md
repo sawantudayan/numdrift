4545fc3468e39e5ccafa83b9b3958d42554a98ad
Udayan Sawant <udayansawant7@gmail.com>
2025-02-05 21:22:57 -0800

    Numdrift - Setup v1.0

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # numdrift
 A high-performance, memory-efficient library for numeric data manipulation with native missing data support.
+
+## Features
+- Native support for missing data
+- Fast performance (at least as good as Pandas)
+- Memory-efficient storage optimized for large datasets
+- Sparse array support for reduced memory footprint
+- Serialization & I/O support
+
+## Installation
+
+To install Numdrift, use pip: 
+```shell script
+pip install numdrift
+```
+
+
+## Usage
+
+```python
+import numdrift as nd
+
+# Create a Numdrift array
+arr = nd.mdarray([1, 2, 3, None, 5])
+
+# Perform operations on the array
+result = arr.mean()
+print(result)
+
+
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='numdrift',
+    version='0.1.0',
+    description='A high-performance, memory-efficient library for numeric data manipulation with native missing data support.',
+    author='Udayan Sawant',
+    author_email='udayansawant7@gmail.com',
+    url='https://github.com/sawantudayan/numdrift',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'numba',
+        'scipy',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
